0c1af940abcc317d036f6f90a35342d2d3fa4cd2
Amankaium <kaium@list.ru>
2021-08-18 23:30:56 +0600

    added travis.yaml and test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomeTestCase(TestCase):
+    def test_home_success(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
